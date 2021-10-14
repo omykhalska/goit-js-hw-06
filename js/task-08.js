6b6a0a7cd2d424713e1d098c8390b73aaa0b66f9
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-14 16:49:59 +0300

    Добавляет решение задания №8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert(`All the fields must be filled in`);
+  }
+
+  console.log({ email: email.value, password: password.value });
+
+  event.currentTarget.reset();
+}
