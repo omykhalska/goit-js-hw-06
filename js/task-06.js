258e49a4e9e2b2244b3ee930310dc0c3e607f562
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-14 14:46:26 +0300

    Вносит правки в задание №6

--- js/task-06.js
+++ js/task-06.js
@@ -4,13 +4,16 @@ textInput.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   let symbolsLength = event.currentTarget.value.length;
-  const validSymbolsLength = event.currentTarget.dataset.length;
+  const validSymbolsLength = Number(event.currentTarget.dataset.length);
 
-  if (symbolsLength != validSymbolsLength) {
-    textInput.classList.add('invalid');
-    textInput.classList.remove('valid');
-  } else {
+  if (symbolsLength === validSymbolsLength) {
     textInput.classList.add('valid');
     textInput.classList.remove('invalid');
+  } else if (symbolsLength === 0) {
+    textInput.classList.remove('valid');
+    textInput.classList.remove('invalid');
+  } else {
+    textInput.classList.add('invalid');
+    textInput.classList.remove('valid');
   }
 }
