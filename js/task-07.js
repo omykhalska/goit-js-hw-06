bcc0887119cf077e2a98a2c459d28a6f57a607a0
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-14 16:06:41 +0300

    Добавляет правки к заданию №7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,14 @@
+const startSize = 56;
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
+
+textEl.style.fontSize = startSize + 'px';
+inputEl.setAttribute('value', startSize);
 
 inputEl.addEventListener('input', onSliderInput);
 
 function onSliderInput(event) {
   const sliderValue = event.currentTarget.value;
-  textEl.style.fontSize = `${sliderValue}px`;
+  textEl.style.fontSize = sliderValue + 'px';
+  inputEl.removeAttribute('value');
 }
