77917ae9493b995dd87a50d8f670bc2239d46661
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-14 12:31:05 +0300

    Добавляет решение задания №4

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', onDecrementBtnClick);
+incrementBtn.addEventListener('click', onIncrementBtnClick);
+
+function onDecrementBtnClick(event) {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+}
+
+function onIncrementBtnClick(event) {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+}
